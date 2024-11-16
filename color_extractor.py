c91372228ff0c7127557e448107834c1d975b419
colungago <121251902+colungago@users.noreply.github.com>
2024-11-16 12:57:37 -0600

    update

--- color_extractor.py
+++ color_extractor.py
@@ -1,13 +1,24 @@
 import colorgram
 
-rgb_colors: list = []
-colors = colorgram.extract('boni.png', 30)
+class ColorExtractor:
+    def __init__(self, path: str, num_of_colors: int):
+        self.path = path
+        self.num_of_colors = num_of_colors
+        self.rgb_colors: list = []
+        self.color_proportions: list = []
+        self.extract_colors()
 
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
+    def extract_colors(self):
+        colors = colorgram.extract(self.path, self.num_of_colors)
+        total_pixels = sum(color.proportion for color in colors)
+        for color in colors:
+            r = color.rgb.r
+            g = color.rgb.g
+            b = color.rgb.b
+            new_color = (r, g, b)
+            self.rgb_colors.append(new_color)
+            proportion = color.proportion / total_pixels
+            self.color_proportions.append((new_color, proportion))
 
-print(rgb_colors)
+    def get_color_proportions(self):
+        return self.color_proportions
---
+++ main.py
@@ -0,0 +1,42 @@
+import turtle as t
+import random
+from color_extractor import ColorExtractor
+
+# Extract colors and their proportions
+color_extractor = ColorExtractor("boni.png", 20)
+color_proportions = color_extractor.get_color_proportions()
+
+# Separate colors and their weights
+colors, weights = zip(*color_proportions)
+
+turtle = t.Turtle()
+screen = t.Screen()
+
+t.colormode(255)
+turtle.speed("fastest")
+turtle.penup()
+turtle.hideturtle()
+
+# Set the starting position
+turtle.setheading(225)
+turtle.forward(300)
+turtle.setheading(0)
+
+number_of_dots = 100 
+dot_size = 20 
+spacing = 50  
+
+for dot_count in range(1, number_of_dots + 1):
+    # Select a color based on its weight
+    color = random.choices(colors, weights)[0]
+    turtle.dot(dot_size, color)
+    turtle.forward(spacing)
+
+    if dot_count % 10 == 0: 
+        turtle.setheading(90)
+        turtle.forward(spacing)
+        turtle.setheading(180)
+        turtle.forward(spacing * 10)
+        turtle.setheading(0)
+
+screen.exitonclick()
